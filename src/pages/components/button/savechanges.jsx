47a6eb4b4d9214f7a9d7006a977cc3b996c7859b
GMw3 <w3ghulam@gmail.com>
2023-01-18 10:05:34 +0500

    Teams: Code Cleanup

--- src/pages/components/button/savechanges.jsx
+++ src/pages/components/button/savechanges.jsx
@@ -6,16 +6,14 @@ import AuthContext from '../../../context/auth';
 export const SaveChanges = (props) => {
   const { check, setCheck } = useContext(AuthContext)
 
-  return (
-    <Box display='flex' justifyContent='flex-end' marginTop='20px'>
-      <Button
-        type="submit"
-        color="secondary"
-        variant="contained"
-        onClick={() => save({ ...props, check, setCheck })}
-      >
-        Save Changes
-      </Button>
-    </Box>
-  )
+  return <Box display='flex' justifyContent='flex-end' marginTop='20px'>
+    <Button
+      type="submit"
+      color="secondary"
+      variant="contained"
+      onClick={() => save({ ...props, check, setCheck })}
+    >
+      Save Changes
+    </Button>
+  </Box>
 }
